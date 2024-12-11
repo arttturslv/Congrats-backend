14d8325daa3bed812b447a5234699bbecda017fb
arttturslv <arttturslv@gmail.com>
2024-12-11 16:50:38 -0300

    feat: added index

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+const globalErrorHandler = require('./controllers/ErrorController');
+const CustomError = require('./utils/CustomError');
+
 
 const app = express();
 
-app.listen(3000);
+app.use(cors());
+app.use(express.json());
+
+const conn = require('./db/conn');
 
 app.get('/', (req, res) => {
     res.send('Hello user')
 })
 
-app.use((req, res, next) => {
-    console.log('Server is 🔥')
+
+const routes = require('./routes/router')
+
+app.use('/api', routes);
+
+app.all('*', (req, res, next) => {
+    const err =  new CustomError(`Can't find ${req.originalUrl} on the server!`, 404)
+    next(err);
 })
+
+app.use(globalErrorHandler)
+
+conn()
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Server is 🔥')
+        });    
+    }).catch((err)=> {
+        console.log("Erro na conexão com o mongo.");
+        console.log(err);
+    });
