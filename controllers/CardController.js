af3d2abce4c75a1042267e056b5d00d9d34ec505
arttturslv <arttturslv@gmail.com>
2024-12-12 16:34:32 -0300

    feat: remove null atributes from request

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -14,13 +14,27 @@ async function checkEasyId (senderName, receiverName) {
     return easyId;
 }
 
+function removeNullAttributes(obj) {
+    if (Array.isArray(obj)) {
+      return obj.map(removeNullAttributes);
+    } else if (obj !== null && typeof obj === 'object') {
+      return Object.fromEntries(
+        Object.entries(obj)
+          .filter(([_, value]) => value !== null) 
+          .map(([key, value]) => [key, removeNullAttributes(value)]) 
+      );
+    }
+    return obj; 
+  }
+
+
 const cardController = {
 
     create:async(req, res, next) => {
         try {
             let easyId = await checkEasyId(req.body.senderName, req.body.receiverName);
             const picturesSliced = req.body.pictures.slice(0,3);
-
+            
             const card = {
                 easyId,
                 title: req.body.title,
@@ -30,7 +44,10 @@ const cardController = {
                 pictures: picturesSliced,
             };
 
-            const response = await cardModel.create(card);
+            const cardNotNull = removeNullAttributes(card)
+            console.log(cardNotNull)
+
+            const response = await cardModel.create(cardNotNull);
 
             res.status(200).json({
                 status: 'success',
@@ -39,7 +56,8 @@ const cardController = {
                 }
             })
         } catch (error) {
-            const err =  new CustomError(error.message, 400)
+            console.log(error)
+            const err =  new CustomError(error, 400)
             next(err);
         }
     },
