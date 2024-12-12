0c7bed1f4a7790badf3531eb3d9c374340185e6f
arttturslv <arttturslv@gmail.com>
2024-12-12 16:34:01 -0300

    feat: increased request size (to allow data64 images)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { PORT } = require('./db/config')
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 
 const conn = require('./db/conn');
 
